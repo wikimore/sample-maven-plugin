e65958526fae66073c9cc0733ae0de63b69c0ba4
tedwang <ted@ximalaya.com>
2014-12-27 19:33:33 +0800

    no msg

--- src/main/java/io/github/wikimore/maven/plugin/CreatedFileMojo.java
+++ src/main/java/io/github/wikimore/maven/plugin/CreatedFileMojo.java
@@ -24,13 +24,14 @@ import java.io.IOException;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
 
 /**
  * @author ted
  */
-@Mojo(name = "createfile")
+@Mojo(name = "createfile", defaultPhase = LifecyclePhase.PACKAGE, threadSafe = true)
 public class CreatedFileMojo extends AbstractMojo {
 
     @Parameter(property = "outPutDirectory", defaultValue = "${project.build.directory}", readonly = true)
